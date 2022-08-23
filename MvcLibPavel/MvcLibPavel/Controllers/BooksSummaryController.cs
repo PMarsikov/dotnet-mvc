c7268bb3d13bc047caa09f0b3ad9715ba7f87e24
Pavel Marsikau <PMarsikau@gomel.iba.by>
2022-08-23 17:25:57 +0300

    bookSummary added

---
+++ MvcLibPavel/MvcLibPavel/Controllers/BooksSummaryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcLibPavel.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace MvcLibPavel.Controllers
+{
+    public class BooksSummaryController : Controller
+    {
+        public static string baseUrl = "https://localhost:7272/api/BooksDetails";
+
+        public async Task<IActionResult> Index()
+        {
+            var getBooksSummary = await GetBooksSummary();
+            return View(getBooksSummary);
+        }
+        ////////////
+        [HttpGet]
+        public async Task<List<BooksSummary>> GetBooksSummary()
+        {
+            //Use the access token to call a protected web API.
+            var accessToken = HttpContext.Session.GetString("JWToken");
+            var url = baseUrl;
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            string jsonStr = await client.GetStringAsync(url);
+
+            var res = JsonConvert.DeserializeObject<List<BooksSummary>>(jsonStr).ToList();
+
+            return res;
+        }
+    }
+}
