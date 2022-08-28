67f33c1caa1dad9d069c8a788a5a2387eea05cb6
Pavel Marsikau <PMarsikau@gomel.iba.by>
2022-08-29 00:31:04 +0300

    polly usage

--- MvcLibPavel/MvcLibPavel/Controllers/BooksSummaryController.cs
+++ MvcLibPavel/MvcLibPavel/Controllers/BooksSummaryController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MvcLibPavel.Models;
 using Newtonsoft.Json;
+using Polly;
 using System.Net.Http.Headers;
 
 namespace MvcLibPavel.Controllers
@@ -31,27 +32,33 @@ namespace MvcLibPavel.Controllers
         [HttpGet]
         public async Task<List<BooksSummary>> GetBooksSummary()
         {
-            //Use the access token to call a protected web API.
-            var accessToken = HttpContext.Session.GetString("JWToken");
-            var url = baseUrl;
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var httpClient = new HttpClient();
+            var maxRetryAttempts = 3;
+            List<BooksSummary> res = new List<BooksSummary>();
+            var retryPolicy = Policy
+                .Handle<HttpRequestException>()
+                .RetryAsync(maxRetryAttempts);
+           
+                await retryPolicy.ExecuteAsync(async () =>
+                {
+                    var accessToken = HttpContext.Session.GetString("JWToken");
+                    var url = baseUrl;
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
-            var response = await client.GetAsync(url);
+                    var response = await httpClient.GetAsync(url);
 
-            try
-            {
-                response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException e)
-            {
-                throw;
-            }
+                    try
+                    {
+                        response.EnsureSuccessStatusCode();
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        throw;
+                    }
 
-            var json = await response.Content.ReadAsStringAsync();
-
-            var res = JsonConvert.DeserializeObject<List<BooksSummary>>(json).ToList();
-
+                    var json = await response.Content.ReadAsStringAsync();
+                    res = JsonConvert.DeserializeObject<List<BooksSummary>>(json).ToList();
+                });
             return res;
         }
     }
