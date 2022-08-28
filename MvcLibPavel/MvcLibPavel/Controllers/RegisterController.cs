3a72c91b13858a8ee4e4e30768ff664f1713cc7d
Pavel Marsikau <PMarsikau@gomel.iba.by>
2022-08-29 02:54:28 +0300

    registration form

---
+++ MvcLibPavel/MvcLibPavel/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcLibPavel.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MvcLibPavel.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> RegisterUser(UserInfo user)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent stringContent = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync("https://localhost:7272/Auth/register", stringContent);
+            }
+            return Redirect("~/Login/Index");
+        }
+    }
+}
